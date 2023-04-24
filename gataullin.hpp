4a23a43097e500250fa9ebcb4e50bbfcb5440efb
Kamil <meowz0771@gmail.com>
2023-04-24 18:51:51 +0300

    num

---
+++ gataullin.hpp
@@ -0,0 +1,6 @@
+int gataullin(int number) {
+    if (number>5) {
+        return number*5;
+    }
+    return number;
+}
--- main.hpp
+++
@@ -1,6 +0,0 @@
-int a(int number) {
-    if (number>5) {
-        return number*number;
-    }
-    return number;
-}
