82096e8d8772ec7a02adeededd3ed984170ae9eb
BruceLee315 <mrpropper315@gmail.com>
2023-04-24 18:30:19 +0300

    file Trofimov.hpp complite WORK

--- Trofimov.hpp
+++ Trofimov.hpp
@@ -1,4 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
+	int num = 0;
+	int result;
+	cout << "Enter number: ";
+	cin >> num;
+	result = num + 2 / 2;
+	cout << result;
+
 	return 0;
 }
